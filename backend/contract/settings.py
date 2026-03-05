59d72bcacac33529e887a3d6ee13563386bb2d41
Ubuntu <ubuntu@ip-172-31-20-237.ec2.internal>
2026-03-05 10:42:28 +0000

    Changes made while deploying

--- backend/contract/settings.py
+++ backend/contract/settings.py
@@ -34,6 +34,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
+    '18.212.212.53',
 ]
 
 
@@ -139,7 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
